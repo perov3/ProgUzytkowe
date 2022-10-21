f27f18c1bed56e86db0890c6b72849a9f3a04831
perov3 <115549645+perov3@users.noreply.github.com>
2022-10-21 11:05:08 +0200

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+//1
+int main()
+{
+//	double x = 5.64;
+//	double y = 5.34;
+//
+//    double suma = x + y;
+//	cout << "Results X + Y " << suma << endl;
+//
+//
+////2
+//
+//
+//	int a, b;
+//	cin >> a >> b;
+//	cout << "Before a= " << a << "b=" << b << endl;
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//	cout << "After: a =" << a << "b=" << b;
+
+//3
+//int main()
+
+	/*double K, C;
+	cout << "Insert temperature to Kelvin \n";
+	 cin*/
+
+//4
+	int a, b;
+	cout << "Podaj A" << endl;
+
+
+//6
+	int liczba;
+	cout << "wprowadz liczbe" << endl;
+	cin >> liczba;
+	if (liczba % 2 == 0)
+		cout << "wpisana liczba jest parzysta" << endl;
+	else
+		cout << "wpisana liczba jest nieparzysta" << endl;
+	return(0);
+
+}
+
+
+
