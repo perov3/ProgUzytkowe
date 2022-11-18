84c131256d0204e1e18a0258e90abd38e3e4600b
perov3 <115549645+perov3@users.noreply.github.com>
2022-11-18 11:00:18 +0100

    Add files via upload

---
+++ cw6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    x[10]; y = 0; z = 0;
+    cout << "Elements of the array";
+        while(z < 20)
+        {
+            x[z] = rand() % 99;
+            cout << x[z] << " ";
+            y = y + x[z];
+            z++;
+        }
+    {
+        cout << "Sum of Elements" << y;
+
+        return 0
+    };
+
+
+
+}
